cb3dd8ddfd1d27e0901ac17368177670f6f28b9a
Tamara Suslova <tsuslova@noveogroup.com>
2016-04-16 18:46:00 +0600

    Project setup

---
+++ race/Model/Race.swift
@@ -0,0 +1,9 @@
+//
+//  Race.swift
+//  Race
+//
+//  Created by Toto on 14.04.16.
+//  Copyright © 2016 Toto. All rights reserved.
+//
+
+import Foundation
---
+++ race/ViewControllers/RaceViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Race
+//
+//  Created by Toto on 14.04.16.
+//  Copyright © 2016 Toto. All rights reserved.
+//
+
+import UIKit
+
+class RaceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
